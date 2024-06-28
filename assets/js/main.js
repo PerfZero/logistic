72107aed35785e8017d11625af292a40ec698eb6
Denis <gundenis1@gmail>
2024-06-28 21:21:12 +0300

    ыы

--- assets/js/main.js
+++ assets/js/main.js
@@ -256,3 +256,47 @@ document.addEventListener('DOMContentLoaded', function() {
     }
   });
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+  let lazyImages = [].slice.call(document.querySelectorAll("img.lazyload"));
+
+  if ("IntersectionObserver" in window) {
+    let lazyImageObserver = new IntersectionObserver(function(entries, observer) {
+      entries.forEach(function(entry) {
+        if (entry.isIntersecting) {
+          let lazyImage = entry.target;
+          lazyImage.src = lazyImage.dataset.src;
+          lazyImage.classList.remove("lazyload");
+          lazyImage.classList.add("lazyloaded");
+          lazyImageObserver.unobserve(lazyImage);
+        }
+      });
+    });
+
+    lazyImages.forEach(function(lazyImage) {
+      lazyImageObserver.observe(lazyImage);
+    });
+  } else {
+    // Fallback для старых браузеров
+    let lazyLoad = function() {
+      let scrollTop = window.pageYOffset;
+      lazyImages.forEach(function(img) {
+        if (img.offsetTop < window.innerHeight + scrollTop) {
+          img.src = img.dataset.src;
+          img.classList.remove('lazyload');
+          img.classList.add('lazyloaded');
+        }
+      });
+
+      if (lazyImages.length == 0) {
+        document.removeEventListener("scroll", lazyLoad);
+        window.removeEventListener("resize", lazyLoad);
+        window.removeEventListener("orientationchange", lazyLoad);
+      }
+    };
+
+    document.addEventListener("scroll", lazyLoad);
+    window.addEventListener("resize", lazyLoad);
+    window.addEventListener("orientationchange", lazyLoad);
+  }
+});
